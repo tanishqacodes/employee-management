1d4ca1f306147ce1728dc9d62983906b9a658b92
tanishqacodes <tanishqasharma797@gmail.com>
2023-09-03 17:15:35 +0530

    set-up

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+
+  return res.send({
+    "hello :" : "tanishqa",
+    "hello :" : "world",
+    "hiii :" : "sid",
+    "hello :" : "tanishqa",
+  });
 });
 
 module.exports = router;
